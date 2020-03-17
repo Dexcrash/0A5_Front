c9dfef6b95940d91f6075e2d5e66b4f59d315351
lala341 <li.forero@uniandes.edu.co>
2020-03-17 04:21:54 -0600

    Cambios estructura

---
+++ a05/lib/assets/colors.dart
@@ -0,0 +1,7 @@
+library color_constants;
+import 'package:flutter/material.dart';
+
+const Color $base = Color(0xffb31830); 
+final Color $secondary = Color(0xff006e68);
+const Color $scaffoldBackground = Color(0xffF2F2F2);
+const Color $white = Colors.white;
