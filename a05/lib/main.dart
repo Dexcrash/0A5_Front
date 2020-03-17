c828289fa935ae9d0538ad6729f98f7915e3836c
lala341 <li.forero@uniandes.edu.co>
2020-03-17 08:35:28 -0600

    Cambios home

--- a05/lib/main.dart
+++ a05/lib/main.dart
@@ -8,6 +8,19 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final Map<int,Color> swatch = 
+  {
+    50: Color.fromRGBO(179,24,48,0.1),
+    100: Color.fromRGBO(179,24,48,0.2),
+    200: Color.fromRGBO(179,24,48,0.3),
+    300: Color.fromRGBO(179,24,48,0.4),
+    400: Color.fromRGBO(179,24,48,0.5),
+    500: Color.fromRGBO(179,24,48,0.6),
+    600: Color.fromRGBO(179,24,48,0.7),
+    700: Color.fromRGBO(179,24,48,0.8),
+    800: Color.fromRGBO(179,24,48,0.9),
+    900: Color.fromRGBO(179,24,48,1)
+  };
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -18,8 +31,8 @@ class MyApp extends StatelessWidget {
         title: 'A05',
         theme: ThemeData(
           // This is the theme of your application.
-          primarySwatch: $secondary,
-          scaffoldBackgroundColor: $scaffoldBackground,
+           primarySwatch: MaterialColor(0xffb31830, swatch),
+         scaffoldBackgroundColor: $scaffoldBackground,
         ),
         initialRoute: '/',
         routes: <String, WidgetBuilder> {
